bb754323a09790015abd989f875e64505a0add02
your_username <sardaralimuradali4@gmail.com>
2023-03-28 23:06:09 +0500

    adding the ads section

--- src/componenets/Blogs.jsx
+++ src/componenets/Blogs.jsx
@@ -2,40 +2,82 @@ import React from "react";
 import "./Blogs.css";
 import { blogsData } from "./data";
 import img from "../assets/trending.png";
-import calender from "../assets/calender.png"
-import time from "../assets/time.png"
+import calender from "../assets/calender.png";
+import time from "../assets/time.png";
 
 const Blogs = () => {
   return (
     <div className="blogsMain">
-      <div className="blogsPart1">
-        {blogsData.map((item) => {
-          return (
-            <div className="singleBlog">
-              <img src={item?.img} />
-              <div className="blogsContent">
-                <div className="travelChip">
+      {/*  */}
+      <div>
+        <div className="blogsPart1">
+          {blogsData.slice(0, 3).map((item) => {
+            return (
+              <div className="singleBlog">
+                <img src={item?.img} />
+                <div className="blogsContent">
+                  <div className="travelChip">
                     <p>Travel</p>
+                  </div>
+                  <div className="trendingFlex">
+                    <img src={img} />
+                    <p>Jenny kiaa</p>
+                  </div>
+                  <div className="trendingTime">
+                    <img src={calender} />
+                    <p>02 december 2022</p>
+                    <img src={time} />
+                    <p>3 min. to read</p>
+                  </div>
+                  <p className="trendingEnd">
+                    Did you come here for something in particular or just
+                    general Riker-bashing? And blowing into maximum warp
+                  </p>
                 </div>
-                <div className="trendingFlex">
-                  <img src={img} />
-                  <p>Jenny kiaa</p>
-                </div>
-                <div className="trendingTime">
-                  <img src={calender} />
-                  <p>02 december 2022</p>
-                  <img src={time} />
-                  <p>3 min. to read</p>
-                </div>
-                <p className="trendingEnd">
-                  Did you come here for something in particular or just general
-                  Riker-bashing? And blowing into maximum warp
-                </p>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
+        <div className="addHere">
+           <h1>Add Here</h1>
+        </div>
+        <div className="blogsPart1">
+          {blogsData.slice(3,-1).map((item) => {
+            return (
+              <div className="singleBlog">
+                <img src={item?.img} />
+                <div className="blogsContent">
+                  <div className="travelChip">
+                    <p>Travel</p>
+                  </div>
+                  <div className="trendingFlex">
+                    <img src={img} />
+                    <p>Jenny kiaa</p>
+                  </div>
+                  <div className="trendingTime">
+                    <img src={calender} />
+                    <p>02 december 2022</p>
+                    <img src={time} />
+                    <p>3 min. to read</p>
+                  </div>
+                  <p className="trendingEnd">
+                    Did you come here for something in particular or just
+                    general Riker-bashing? And blowing into maximum warp
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
+      {/*  */}
+      
+      <div className="BlogsPart2">
+        Here Come the Right Repeated Side Bar
+      </div>
+
+
+
     </div>
   );
 };
--- src/componenets/Trending.jsx
+++ src/componenets/Trending.jsx
@@ -10,21 +10,6 @@ const Trending = () => {
     <div className='trendingMain'>
         <div className='trendingText'>
             <h2>tranding</h2>
-            {/* <div className='trendingContainer'>
-                <a className='trendingHead'>I Created a Developer Rap Video -</a>
-                <a className='trendingHead'>Here's What I Learned</a>
-                <div className='trendingFlex'>
-                    <img src={img}/>
-                    <p>Jenny kiaa</p>
-                </div>
-                <div className='trendingTime'>
-                    <img src={calender}/>
-                    <p>02 december 2022</p>
-                    <img src={time}/>
-                    <p>3 min. to read</p>
-                </div>
-                <p className='trendingEnd'>Did you come here for something in particular or just general Riker-bashing? And blowing into maximum warp</p>
-            </div> */}
 
             <div className='trendingGrid'>
                {trendingData.map((item)=>{
